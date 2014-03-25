041b775301d28a4c421c9b6c6546954e768f9538
Jacob Roeland <jacob@jacroe.com>
2014-03-25 18:21:15 -0500

    Don't send signed-in users to homepage

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ require "scalene/Scalene.php";
 
 $_->load->core("users");
 if ($user = $_->users->userLoggedIn())
-	$data["user"] = $_->users->getUser();
+	header("Location: settings.php");
 
-$_->view->display("index", $data);
+$_->view->display("index");
