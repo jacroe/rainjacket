4b8d2befbfea44cf7021182da26cfa8f6eb5f827
Jacob Roeland <jacob@jacroe.com>
2014-07-17 23:57:50 -0400

    (scalene) Addition of a new function from a long time ago

--- scalene/helpers/strings_helper.php
+++ scalene/helpers/strings_helper.php
@@ -10,3 +10,11 @@ if (!function_exists("pluralize"))
 			return ($count == 1) ? $singlular : $singlular."s";
 	}
 }
+
+if (!function_exists("camelize"))
+{
+	function camelize($str)
+	{
+		return strtolower($str[0]).substr(str_replace(' ', '', ucwords(preg_replace('/[\s_]+/', ' ', $str))), 1);
+	}
+}
